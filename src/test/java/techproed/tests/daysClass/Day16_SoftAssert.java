2bd534fa2ca121b0450dc6f919d53e0f73f92999
tlzf <fzltcetin@gmail.com>
2023-01-18 17:15:58 -0600

    day16update

--- src/test/java/techproed/tests/daysClass/Day16_SoftAssert.java
+++ src/test/java/techproed/tests/daysClass/Day16_SoftAssert.java
@@ -1,13 +1,20 @@
-package techproed.tests.daysClass;
+package techproed.tests;
+
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
 public class Day16_SoftAssert {
     @Test
     public void softAssertTest(){
         SoftAssert softAssert = new SoftAssert();
+
         System.out.println("Line 11 Code");
-        softAssert.assertEquals(3,5);
+        softAssert.assertEquals(3,5);//FAILS
         System.out.println("Line 13 Code");
-        softAssert.assertAll();
+        softAssert.assertEquals("apple","orange");//FAILS
+        System.out.println("Line 15 Code");
+        softAssert.assertTrue(false);//FAILS
+        System.out.println("Line 17 Code");
+        softAssert.assertAll();//Marking the test case as FAILED or PASSED
     }
 }
