ebea62d4a46d2f1a579d419156f94fd2b52ceef2
tlzf <fzltcetin@gmail.com>
2023-01-18 19:18:49 -0600

    config

--- src/test/java/techproed/tests/daysClass/MedunnaPatient.java
+++ src/test/java/techproed/tests/daysClass/MedunnaPatient.java
@@ -1,7 +1,47 @@
 package techproed.tests.daysClass;
 
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
 public class MedunnaPatient {
+    //Admin can create patients
+    /*
+    Given
+        Go to https://medunna.com/
+    When
+        Click on User icon
+    And
+        Click on "Sign In" section
+    And
+        Enter username into "Username" input
+    And
+        Enter password into "Password" input
+    And
+        Check the "Remember Me" checkbox
+    And
+        Click on "Sign In" button
+    And
+        Click on "Items & Titles" menu
+    And
+        Click on "Patient" section
+    And
+        Click on "Create a new patient" button
+    And
+        Enter firstname into "First Name" input
+    And
+        Enter last name into "Last Name" input
+    And
+        Enter email into "Email" input
+    And
+        Enter phone number into "Phone" input
+    And
+        Click on "Save" button
+     */
+    @Test
+    public void createPatientTest(){
 
-
+        Driver.getDriver().get(ConfigReader.getProperty(""));
+    }
 
 }
